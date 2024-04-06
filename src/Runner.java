50608a09f2462e89c7e2240c2d7d2f07eae8b746
goodv1ce <andriy.lysak.2003@gmail.com>
2024-04-06 16:51:14 +0300

    [ADD] added entry point Runner.java

---
+++ src/Runner.java
@@ -0,0 +1,8 @@
+import util.Function;
+
+public class Runner {
+    public static void main(String[] args) {
+        new Function().calculateFirstFunction();
+        new Function().calculateSecondFunction();
+    }
+}
