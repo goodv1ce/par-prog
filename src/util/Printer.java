a506cf73c6c5a1884e25f56d646390ee29f114b5
goodv1ce <andriy.lysak.2003@gmail.com>
2024-04-06 16:49:20 +0300

    [ADD] added Printer class for printing matrices and vectors

--- src/util/Printer.java
+++ src/util/Printer.java
@@ -10,4 +10,25 @@ public class Printer {
             }
         }
     }
+
+    public static void printVectorInBounds(double[] vector, int a, int b) {
+        for (int i = a; i < b; i++) {
+            System.out.print(vector[i]);
+            if (i != b - 1) {
+                System.out.print(", ");
+            }
+        }
+    }
+
+    public static void printMatrix(double[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        for (double[] doubles : matrix) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(doubles[j] + "\t");
+            }
+            System.out.println();
+        }
+    }
 }
